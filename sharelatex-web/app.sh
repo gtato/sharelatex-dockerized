14bf41503f53bd5ce4643a7b418f70c692fc2f02
Genc <gtato6@gmail.com>
2018-06-07 16:01:17 +0200

    support static service references

--- sharelatex-web/app.sh
+++ sharelatex-web/app.sh
@@ -8,8 +8,17 @@
 #     SHARELATEX_REAL_TIME_URL="http://real-time-edge:3026"
 # fi
 
+[ -z "$NGINX_CONFIG" ] && NGINX_CONFIG="DEFAULT"
 [ -z "$SHARELATEX_REAL_TIME_URL" ] && SHARELATEX_REAL_TIME_URL="http://real-time:3026"
-[ -z "$SHARELATEX_WEB_URL" ] && SHARELATEX_WEB_ALT_URL="http://web:80"
+[ -z "$SHARELATEX_WEB_URL" ] && SHARELATEX_WEB_URL="http://web:80"
+
+
+if [ "$NGINX_CONFIG" = "KOALA" ]
+then 
+cp /app/sharelatex_koala.conf /etc/nginx/sites-enabled/sharelatex.conf
+else
+cp /app/sharelatex_def.conf /etc/nginx/sites-enabled/sharelatex.conf
+fi
 
 
 sed -i 's@SHARELATEX_REAL_TIME_URL@'"$SHARELATEX_REAL_TIME_URL"'@g' /etc/nginx/sites-enabled/sharelatex.conf
