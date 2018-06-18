cb8e5d4848b6765b33b47829b6f0d26d64097108
Genc <gtato6@gmail.com>
2018-06-18 18:34:55 +0200

    updated check to be a bit more specific

--- check.py
+++ check.py
@@ -3,19 +3,46 @@ import urllib2
 import time
 import sys
 import os
+import json
+
 
 def main():
-    for i in range(2):
+    cont_imgs = os.popen('docker ps --format "{{.Image}}"').read()
+    imgs_lines = cont_imgs.split('\n')
+    shl_services = []
+    for img in imgs_lines:
+        if 'sharelatex' in img and 'redis-syncer' not in img:
+            img_clean = img.replace('gtato/', '')
+            if img_clean == 'sharelatex-web':
+                img_clean = 'sharelatex-web-80'
+            shl_services.append(img_clean )
+    print shl_services
+
+    contents = []
+    for i in range(5):
+        time.sleep(1)
         try:
-            contents = urllib2.urlopen("http://localhost:8008/").read()
-            if 'No services registered yet' not in contents:
-                sys.exit(0)
+            contents = json.loads(urllib2.urlopen("http://localhost:8008/api/list/local").read())
+            if len(contents) == 0:
+                print 'services not registered yet'
+            else:
+                break
         except:
             print 'koala is not up yet'
-        time.sleep(2)
 
-    print 'restarting registrator'
-    os.popen('docker-compose -f docker-compose-koala-core.yml restart registrator')  
+    # ok now what is there is there
+    registered = []
+    for s in contents:
+        registered.append(s['name'])
+
+    nr_not_registered = 0
+    for n in shl_services:
+        if n not in registered:
+            nr_not_registered += 1
+
+    if nr_not_registered > 0:
+        print 'restarting registrator'
+        os.popen('docker-compose -f docker-compose-koala-core.yml restart registrator')
 
 if __name__ == "__main__":
     main()
